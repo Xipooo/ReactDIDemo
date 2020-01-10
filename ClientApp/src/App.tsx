eafa97653252647c425264c7cc03fd077d29f127
Steve Bishop <ringersring@hotmail.com>
2020-01-10 12:54:45 -0800

    Provide the container to the component tree

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -4,13 +4,16 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
-
+import { Provider } from 'inversify-react';
+import { container } from './inversify.config';
 import './custom.css'
 
 export default () => (
-    <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-    </Layout>
+    <Provider container={container}>
+        <Layout>
+            <Route exact path='/' component={Home} />
+            <Route path='/counter' component={Counter} />
+            <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+        </Layout>
+    </Provider>
 );
